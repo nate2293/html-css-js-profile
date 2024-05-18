5630c808a194189865bca025c99d7b338d236b48
nate2293 <Mc_Garvey-N@ulster.ac.uk>
2024-05-18 22:16:53 +0100

    profile
    profile photo added, linkedIn added and also resume needs adjusted to be directed to my CV
    
    Template at the moment.

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+/* targeting menu links and hamburger icon, when we click it will open the stytling  */
